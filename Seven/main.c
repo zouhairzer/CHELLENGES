3b3094a499c633ae012456d9368b081d843f4563
zerzkhane <zerzkhanezouhair.com>
2023-09-14 11:32:46 +0100

    Seven

---
+++ Seven/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num [] = {2, 3, 4};
+    printf("%d",num[2]);
+    printf("%d",num[1]);
+    printf("%d",num[0]);
+    return 0;
+}
