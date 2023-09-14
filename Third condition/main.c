4c87ba82a76d8933a5946384c3e80b5bdc82c8f4
zerzkhane <zerzkhanezouhair.com>
2023-09-14 13:27:27 +0100

    third

---
+++ Third condition/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    float somme;
+    printf("ecrit la valeur de A : ");
+    scanf("%d", &a);
+
+    printf("ecrit la valeur de B : ");
+    scanf("%d", &b);
+
+    if (a = b ){
+     somme = (a+b)*3;
+    printf("%f", somme);
+    }
+
+
+    return 0;
+}
