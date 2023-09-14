699bcb7e64de446b76a4aae1aa6cacbf6276b3a0
zerzkhane <zerzkhanezouhair.com>
2023-09-14 13:14:27 +0100

    second_condition

---
+++ second condition/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+   char voyelle = 'B';
+
+   switch (voyelle){
+
+   case 'A':
+       printf("voyelle");
+       break;
+   case 'B':
+    printf("non");
+    break;
+   }
+
+    return 0;
+}
